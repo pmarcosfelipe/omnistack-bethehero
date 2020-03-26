236a59e5ba323256bdeb508cf8265b807306d9c9
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-03-25 23:17:46 -0300

    Create Incident Entity and their methods: post and get

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
-  async getAll(request, response) {
+  async list(request, response) {
     const ongs = await connection('ongs').select('*');
 
     return response.json(ongs);
