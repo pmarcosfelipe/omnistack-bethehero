4023cebe2ffe0ca82fb5b95966e7aeb365588554
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-03-28 00:07:46 -0300

    Created Logout method redirect

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -9,6 +9,8 @@ import logoImg from '../../assets/logo.svg';
 import './styles.css';
 
 export default function Profile() {
+  const history = useHistory();
+
   const ongID = localStorage.getItem('ongID');
   const ongName = localStorage.getItem('ongName');
 
@@ -38,6 +40,11 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -49,7 +56,7 @@ export default function Profile() {
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041"></FiPower>
         </button>
       </header>
