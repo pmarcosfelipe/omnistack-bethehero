45f79573aafe4c011b5119060683aa005b1b5cf8
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-03-29 02:33:42 -0300

    Configuring JEST and create a unit test to generateUniqueID function

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueID = require('../utils/generateUniqueID');
 
 const connection = require('../database/connection');
 
@@ -12,7 +12,7 @@ module.exports = {
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueID();
 
     await connection('ongs').insert({
       id,
